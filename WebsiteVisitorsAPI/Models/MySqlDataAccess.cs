ec0111976bcebe60f2301db5cf293583bc9c4df5
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-02 11:48:28 +0200

    MySqlDataAccess class added and implemented.

---
+++ WebsiteVisitorsAPI/Models/MySqlDataAccess.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using WebsiteVisitorsAPI.Models.Interfaces;
+using System.Data;
+using Dapper;
+
+namespace WebsiteVisitorsAPI.Models
+{
+    public class MySqlDataAccess : IDataAccess
+    {
+        private readonly IConfiguration _config;
+        public string ConnectionType { get; set; } = "Default";
+        
+        public MySqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+        public IEnumerable<T> LoadData<T, U>(string sqlQuery, U parameters)
+        {
+            IEnumerable<T> output;
+            using (IDbConnection db = new MySqlConnection(_config.GetConnectionString(ConnectionType)))
+            {
+                output = db.Query<T>(sqlQuery, parameters);
+                return output;
+            }
+
+        }
+
+        public void SaveData<T>(string sqlQuery, T parameters)
+        {
+            using (IDbConnection db = new MySqlConnection(_config.GetConnectionString(ConnectionType)))
+            {
+                db.Execute(sqlQuery, parameters);
+            }
+        }
+    }
+}
