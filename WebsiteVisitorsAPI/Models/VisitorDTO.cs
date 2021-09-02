58f5f90eb7910cfa1dd662ec121c0bf8cb35fec2
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-02 13:55:36 +0200

    VisitorDTO data annotations added.

--- WebsiteVisitorsAPI/Models/VisitorDTO.cs
+++ WebsiteVisitorsAPI/Models/VisitorDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using WebsiteVisitorsAPI.Models.Interfaces;
@@ -8,8 +9,12 @@ namespace WebsiteVisitorsAPI.Models
 {
     public class VisitorDTO : IVisitor
     {
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
         public string LastName { get; set; }
+        [Required]
+        [StringLength(20)]
         public string City { get; set; }
     }
 }
