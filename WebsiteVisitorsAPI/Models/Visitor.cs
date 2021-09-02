c934d65bf36e27ad385e9eac97dc3bbc1c9c7a00
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-02 11:57:00 +0200

    Visitor class added and implemented.

---
+++ WebsiteVisitorsAPI/Models/Visitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteVisitorsAPI.Models.Interfaces;
+
+namespace WebsiteVisitorsAPI.Models
+{
+    public class Visitor : IVisitor
+    {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
