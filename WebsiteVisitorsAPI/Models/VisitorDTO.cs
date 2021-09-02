d003b36bda06ccd81a887a3481e18b78157034d0
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-02 11:58:57 +0200

    VisistorDTO class added and implemented.

---
+++ WebsiteVisitorsAPI/Models/VisitorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteVisitorsAPI.Models.Interfaces;
+
+namespace WebsiteVisitorsAPI.Models
+{
+    public class VisitorDTO : IVisitor
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
