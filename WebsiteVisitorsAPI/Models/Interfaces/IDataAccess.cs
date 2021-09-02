b7d7675090b3cf35f9d55ea257e958325f50e90a
Wojciech Larecki <wojtek.larecki@gmail.com>
2021-09-02 11:32:56 +0200

    IDataAccess interface added and implemented

---
+++ WebsiteVisitorsAPI/Models/Interfaces/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteVisitorsAPI.Models.Interfaces
+{
+    interface IDataAccess
+    {
+        IEnumerable<T> LoadData<T, U>(string sqlQuery, U parameters);
+        void SaveData<T>(string sqlQuery, T parameters);
+
+    }
+}
